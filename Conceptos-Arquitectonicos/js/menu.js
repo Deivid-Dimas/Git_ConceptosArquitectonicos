2d7c560d7eb29418fbc384526d971ba8ab6d8c67
Deivid-Dimas <soporteneca@outlook.com>
2024-01-12 15:48:15 -0500

    menu and slider

---
+++ Conceptos-Arquitectonicos/js/menu.js
@@ -0,0 +1,13 @@
+// $(window).scroll(function() {
+//     var scroll = $(window).scrollTop();
+//     if (scroll >= 50) { // change this value according to when you want to add the class
+//         $(".main-header").addClass("fixed-nav");
+//     } else {
+//         $(".main-header").removeClass("fixed-nav");
+//     }
+// });
+
+// $('#mainMenu li a').on('click', function() {
+//     $('#mainMenu li a').removeClass('active');
+//     $(this).addClass('active');
+// });
